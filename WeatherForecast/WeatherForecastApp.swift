cefd2ef5f47c469614ab5e14230be634047dc8a7
Woo Sung Jahng <jlittle0919@icloud.com>
2024-08-28 18:54:40 +0900

    Initial commit

---
+++ WeatherForecast/WeatherForecastApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherForecastApp.swift
+//  WeatherForecast
+//
+//  Created by Woo Sung Jahng on 2024/04/17.
+//
+
+import SwiftUI
+
+@main
+struct WeatherForecastApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
